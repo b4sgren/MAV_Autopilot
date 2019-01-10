7a7aece734f229418bf0c2670f2b8892c22a0318
Brendon Forsgren <bforsgren29@gmail.com>
2019-01-10 16:39:13 -0700

    Successfully pulls up a window. It doesn't stay but you can see it flash up

---
+++ mav_viewer.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pyqtgraph as pg
+import pyqtgraph.opengl as gl
+import pyqtgraph.Vector as Vector
+
+class MAV_Viewer:
+    def __init__(self):
+        self.application = pg.QtGui.QApplication([])
+        self.window = gl.GLViewWidget()
+        self.window.setWindowTitle('Flight Simulator')
+        self.window.setGeometry(0, 0, 1000, 1000)
+        grid = gl.GLGridItem()
+        grid.scale(20, 20, 20)
+        self.window.addItem(grid)
+        self.window.setCameraPosition(distance=200)
+        self.window.setBackgroundColor('k')
+        self.window.show()
+        self.window.raise_()
+        self.plot_initialize = False
+
+if __name__ == "__main__":
+    simulator = MAV_Viewer()
