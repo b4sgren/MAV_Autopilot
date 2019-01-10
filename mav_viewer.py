b60f4e571d3bc3fe50ca704d2aadc9d971e9790b
Brendon Forsgren <bforsgren29@gmail.com>
2019-01-10 16:58:20 -0700

    Added code to draw a box. The box isn't visible yet but that is the next step

--- mav_viewer.py
+++ mav_viewer.py
@@ -17,7 +17,43 @@ class MAV_Viewer:
         self.window.show()
         self.window.raise_()
         self.plot_initialize = False
+        self.points, self.mesh_colors = self.getMAVPoints()
 
+    def getMAVPoints(self):
+        points = np.array([[1, 1, 0],
+                           [1, -1, 0],
+                           [-1, -1, 0],
+                           [-1, 1, 0],
+                           [1, 1, -2],
+                           [1, -1, -2],
+                           [-1, -1, -2],
+                           [-1, 1, -2],
+                           [1.5, 1.5, 0],
+                           [1.5, -1.5, 0],
+                           [-1.5, -1.5, 0],
+                           [-1.5, 1.5, 0]]).T
+        scale = 10
+        points = scale * points
+
+        red = np.array([1.0, 0.0, 0.0, 1])
+        green = np.array([0.0, 1.0, 0.0, 1])
+        blue = np.array([0.0, 0.0, 1.0, 1])
+        yellow = np.array([1.0, 1.0, 0.0, 1])
+        mesh_colors = np.empty((12, 3, 4), dtype=np.float32)
+        mesh_colors[0] = yellow
+        mesh_colors[1] = yellow
+        mesh_colors[2] = blue
+        mesh_colors[3] = blue
+        mesh_colors[4] = blue
+        mesh_colors[5] = blue
+        mesh_colors[6] = blue
+        mesh_colors[7] = blue
+        mesh_colors[8] = blue
+        mesh_colors[9] = blue
+        mesh_colors[10] = green
+        mesh_colors[11] = green
+
+        return points, mesh_colors
 if __name__ == "__main__":
     simulator = MAV_Viewer()
     pg.QtGui.QApplication.instance().exec_()
