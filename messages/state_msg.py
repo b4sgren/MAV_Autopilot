247bbe123ccfba3155c7e3c57be58b2188194537
Brendon Forsgren <bforsgren29@gmail.com>
2019-01-22 15:57:16 -0700

    Added the other states to the state message to get it working

--- messages/state_msg.py
+++ messages/state_msg.py
@@ -8,3 +8,17 @@ class StateMsg:
         self.phi = 0.0      #roll angle
         self.theta = 0.0    #pitch angle
         self.psi = 0.0      #yaw angle
+        self.Va = 0.0       # airspeed in m/s
+        self.alpha = 0.0    #angle of attack in rad
+        self.beta = 0.0     #sideslip angle in radians
+        self.p = 0.0        #roll rate in rad/s
+        self.q = 0.0        #pitch rate in rad/s
+        self.r = 0.0        #yaw rate in rad/s
+        self.Vg = 0.0       #groundspeed in m/s
+        self.gamma = 0.0    #flgith path angle in rad
+        self.chi = 0.0      # course angle in radians
+        self.wn = 0.0       #inertial windspeed in north direction m/s
+        self.we = 0.0       #inertial windspeed in east direction m/s
+        self.bx = 0.0       #gyro bias along roll axis in rad/s
+        self.by = 0.0       #gyro bias along pitch axis in rad/s
+        self.bz = 0.0       #gyro bias along yaw axis in rad/s
