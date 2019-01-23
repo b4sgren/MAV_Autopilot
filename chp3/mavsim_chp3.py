82d2e6e7b77ac1ee8e0dcb9567f29037e8851b1a
Brendon Forsgren <bforsgren29@gmail.com>
2019-01-23 10:49:29 -0700

    Started updating the simulation file

--- chp3/mavsim_chp3.py
+++ chp3/mavsim_chp3.py
@@ -8,46 +8,39 @@ import sys
 sys.path.append('..')
 
 # import viewers and video writer
-from chap2.spacecraft_viewer import spacecraft_viewer
-from chap2.video_writer import video_writer
+from mav_viewer import MAV_viewer
+from video_writer import video_writer
 
 # import parameters
-import parameters.simulation_parameters as SIM
+import parameters.sim_params as SIM
 # import message types
-from message_types.msg_state import msg_state
+from messages.state_msg import StateMsg 
 
 # initialize messages
-state = msg_state()  # instantiate state message
+state = StateMsg()  # instantiate state message
 
 # initialize viewers and video
 VIDEO = False  # True==write video, False==don't write video
-spacecraft_view = spacecraft_viewer()
+mav_view = MAV_viewer()
 if VIDEO == True:
     video = video_writer(video_name="chap2_video.avi",
                          bounding_box=(0, 0, 1000, 1000),
                          output_rate=SIM.ts_video)
 
 # initialize the simulation time
-sim_time = SIM.start_time
+sim_time = SIM.t0
 
 # main simulation loop
-while sim_time < SIM.end_time:
-    #-------vary states to check viewer-------------
-    if sim_time < SIM.end_time/6:
-        state.pn += 10*SIM.ts_simulation
-    elif sim_time < 2*SIM.end_time/6:
-        state.pe += 10*SIM.ts_simulation
-    elif sim_time < 3*SIM.end_time/6:
-        state.h += 10*SIM.ts_simulation
-    elif sim_time < 4*SIM.end_time/6:
-        state.psi += 0.1*SIM.ts_simulation
-    elif sim_time < 5*SIM.end_time/6:
-        state.theta += 0.1*SIM.ts_simulation
-    else:
-        state.phi += 0.1*SIM.ts_simulation
-
+while sim_time < SIM.t_end:
+    #-------vary forces to check viewer-------------
+    fx = 0
+    fy = 0
+    fz = 0
+    l = 0
+    m = 0
+    n = 0
     #-------update viewer and video-------------
-    spacecraft_view.update(state)
+    mav_view.update(state)
     if VIDEO == True: video.update(sim_time)
 
     #-------increment time-------------
