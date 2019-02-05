53cb5e8a6d6a043492dbe6ca8ba3e3f7ce6b0684
Brendon Forsgren <bforsgren29@gmail.com>
2019-02-04 18:38:05 -0700

    Have the correct sizes of matrices for A, B, and C. Now to set them up

--- chp4/wind_simulation.py
+++ chp4/wind_simulation.py
@@ -11,17 +11,17 @@ import parameters.aerosonde_parameters as MAV
 class wind_simulation:
     def __init__(self, Ts):
         # steady state wind defined in the inertial frame
-        # self._steady_state = np.array([[0., 0., 0.]]).T
-        self._steady_state = np.array([[3., 1., 0.]]).T
+        self._steady_state = np.array([[0., 0., 0.]]).T
+        # self._steady_state = np.array([[3., 1., 0.]]).T
 
         #   Dryden gust model parameters (pg 56 UAV book)
         # HACK:  Setting Va to a constant value is a hack.  We set a nominal airspeed for the gust model.
         # Could pass current Va into the gust function and recalculate A and B matrices.
         Va = 17
-        self._A = np.eye(3) # Create A and B from TF in the book. See slides online on Chp. 4
-        self._B = np.eye(3) # Not sure how to fix the hack or what it means
-        self._C = np.eye(3)
-        self._gust_state = np.zeros((3, 1))
+        self._A = np.zeros((5, 5)) # Create A and B from TF in the book. See slides online on Chp. 4
+        self._B = np.zeros((5, 3)) # Not sure how to fix the hack or what it means
+        self._C = np.zeros((3, 5))
+        self._gust_state = np.zeros((5, 1))
         self._Ts = Ts
 
     def update(self):
