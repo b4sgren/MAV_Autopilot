Fixed the gravity. The rotation matrix was body to inertial and needed to be inertial to body. I just transposed the matrix

chp4/mav_dynamics.py
@@ -168,16 +168,16 @@ class mav_dynamics:
 
     def calcForcesAndMoments(self, delta):
         # Calculate gravitational forces in the body frame
-        fb_grav = Quaternion2Rotation(self._state[6:10]) @ np.array([[0, 0, MAV.mass * MAV.gravity]]).T
+        fb_grav = Quaternion2Rotation(self._state[6:10]).T @ np.array([[0, 0, MAV.mass * MAV.gravity]]).T
 
         # Calculating longitudinal forces and moments
         fx, fz, m = self.calcLongitudinalForcesAndMoments(delta.item(0))
-        # fx += fb_grav[0] #This makes weird stuff happen right now 
-        # fz += fb_grav[2]
+        fx += fb_grav[0] #This makes weird stuff happen right now
+        fz += fb_grav[2]
 
         # Calculating lateral forces and moments
         fy, l, n = self.calcLateralForcesAndMoments(delta.item(2), delta.item(3))
-        # fy += fb_grav[1]
+        fy += fb_grav[1]
 
         # Propeller force and moments
         fp, mp = self.calcThrustForceAndMoment(delta.item(1))

chp4/wind_simulation.py
@@ -11,8 +11,8 @@ import parameters.aerosonde_parameters as MAV
 class wind_simulation:
     def __init__(self, Ts):
         # steady state wind defined in the inertial frame
-        # self._steady_state = np.array([[0., 0., 0.]]).T
-        self._steady_state = np.array([[3., 1., 0.]]).T
+        self._steady_state = np.array([[0., 0., 0.]]).T
+        # self._steady_state = np.array([[3., 1., 0.]]).T
 
         #Dryden Parameters:
         self.Lu = 200.0
@@ -46,7 +46,8 @@ class wind_simulation:
         # returns a six vector.
         #   The first three elements are the steady state wind in the inertial frame
         #   The second three elements are the gust in the body frame
-        return np.concatenate(( self._steady_state, self._gust(Va) ))
+        # return np.concatenate(( self._steady_state, self._gust(Va) ))
+        return np.concatenate(( self._steady_state, np.zeros((3, 1)) ))
 
     def _gust(self, Va):
         self._A = np.array([[-Va/self.Lu, 0, 0, 0, 0],