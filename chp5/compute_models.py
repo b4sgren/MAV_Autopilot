d9e5540d0704b967d474144e0ac1b2bf19dfb045
Brendon Forsgren <bforsgren29@gmail.com>
2019-02-14 12:09:11 -0700

    First 2 transfer function models done

--- chp5/compute_models.py
+++ chp5/compute_models.py
@@ -23,13 +23,22 @@ def compute_tf_model(mav, trim_state, trim_input):
     Va = mav._Va
     b = MAV.b
 
-    # trim values
+    # Transfer Function Models
     b2Va = b/(2 * Va)
     a_phi_1 = 0.5 * rho * (Va**2) * S * b * MAV.C_p_p * b2Va
     a_phi_2 = 0.5 * rho * (Va**2) * S * b * MAV.C_p_delta_a
     T_phi_delta_a = TF(np.array([a_phi_2]), np.array([1, a_phi_1, 0]))
 
-    return T_phi_delta_a, T_chi_phi, T_theta_delta_e, T_h_theta, T_h_Va, T_Va_delta_t, T_Va_theta, T_beta_delta_r
+    T_chi_phi = TF(np.array([MAV.gravity/mav._Va]), np.array([1, 0]))
+
+    T_theta_delta_e = 0
+    T_h_theta = 0
+    T_h_Va = 0
+    T_Va_delta_t = 0
+    T_Va_theta = 0
+    T_beta_delta_r = 0
+
+    return [T_phi_delta_a, T_chi_phi, T_theta_delta_e, T_h_theta, T_h_Va, T_Va_delta_t, T_Va_theta, T_beta_delta_r]
 
 def compute_ss_model(mav, trim_state, trim_input):
      return A_lon, B_lon, A_lat, B_lat
@@ -66,11 +75,11 @@ def dT_ddelta_t(mav, Va, delta_t):
     return dThrust
 
 if __name__ == "__main__":
-    mav = Dynamics(Ts)
+    mav = Dynamics(0.02)
     Va = 25.0
     gamma = 0.0
     mav._Va = Va
 
     trim_state, trim_input = compute_trim(mav, Va, gamma)
 
-    T_phi_delta_a, T_chi_phi, T_theta_delta_e, T_h_theta, T_h_Va, T_Va_delta_t, T_Va_theta, T_beta_delta_r = compute_tf_model(mav, trim_state, trim_input)
+    tf_list = compute_tf_model(mav, trim_state, trim_input)
