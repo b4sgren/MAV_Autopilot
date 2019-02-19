e4d6d03a2dd8498f9554be3c4d6547b822d08ae0
Brendon Forsgren <bforsgren29@gmail.com>
2019-02-19 16:05:02 -0700

    Copied a few more files over

--- parameters/control_parameters.py
+++ parameters/control_parameters.py
@@ -17,36 +17,36 @@ a_theta1 = data[6]
 a_theta2 = data[7]
 a_theta3 = data[8]
 
-# gravity =
-# sigma =
-# Va0 =
-#
-# #----------roll loop-------------
-# roll_kp =
-# roll_kd =
-#
-# #----------course loop-------------
-# course_kp =
-# course_ki =
-#
-# #----------sideslip loop-------------
-# sideslip_ki =
-# sideslip_kp =
-#
-# #----------yaw damper-------------
-# yaw_damper_tau_r =
-# yaw_damper_kp =
-#
-# #----------pitch loop-------------
-# pitch_kp =
-# pitch_kd =
-# K_theta_DC =
-#
-# #----------altitude loop-------------
-# altitude_kp =
-# altitude_ki =
-# altitude_zone =
-#
-# #---------airspeed hold using throttle---------------
-# airspeed_throttle_kp =
-# airspeed_throttle_ki =
+gravity = 9.8
+sigma =
+Va0 =
+
+#----------roll loop-------------
+roll_kp =
+roll_kd =
+
+#----------course loop-------------
+course_kp =
+course_ki =
+
+#----------sideslip loop-------------
+sideslip_ki =
+sideslip_kp =
+
+#----------yaw damper-------------
+yaw_damper_tau_r =
+yaw_damper_kp =
+
+#----------pitch loop-------------
+pitch_kp =
+pitch_kd =
+K_theta_DC =
+
+#----------altitude loop-------------
+altitude_kp =
+altitude_ki =
+altitude_zone =
+
+#---------airspeed hold using throttle---------------
+airspeed_throttle_kp =
+airspeed_throttle_ki =
