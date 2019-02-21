Pretty sure calculation of chi is correct. Controller is still weird

chp6/autopilot.py
@@ -58,7 +58,7 @@ class autopilot:
 
         # lateral autopilot
         phi_c =   self.course_from_roll.update(cmd.course_command, state.chi)  #20 * np.pi / 180.
-        print(phi_c)
+        print(state.chi)
         delta_a = self.roll_from_aileron.update_with_rate(phi_c, state.phi, state.p)  # AP.trim_input.item(2)
         delta_r = AP.trim_input.item(3)
 

chp6/mav_dynamics.py
@@ -7,7 +7,7 @@ mav_dynamics
 import sys
 sys.path.append('..')
 import numpy as np
-from math import asin, exp
+from math import asin, exp, acos
 
 # load message types
 from messages.state_msg import StateMsg
@@ -144,8 +144,27 @@ class mav_dynamics:
         self.msg_true_state.alpha = self._alpha  # see line 164 updateVelocityData
         self.msg_true_state.beta = self._beta  # see line 167 updateVelocityData
         self.msg_true_state.Vg = np.linalg.norm(self._state[3:6])
-        self.msg_true_state.gamma = np.arctan2(-self._state.item(5), self._state.item(3)) # is this right atan2(-w, u)
-        self.msg_true_state.chi = np.arctan2(self._state.item(4), self._state.item(3)) + psi # atan2(v, u) Not sure this is right
+        self.calcGammaAndChi()
+        # self.msg_true_state.gamma = np.arctan2(-self._state.item(5), self._state.item(3)) # is this right atan2(-w, u)
+        # self.msg_true_state.chi = np.arctan2(self._state.item(4), self._state.item(3)) + psi # atan2(v, u) Not sure this is right
+
+    def calcGammaAndChi(self):
+        Rv_b = Quaternion2Rotation(self._state[6:10])
+        Vg = Rv_b @ self._state[3:6]
+
+        gamma = asin(-Vg.item(2)/np.linalg.norm(Vg)) #negative because h_dot = Vg sin(gamma)
+        self.msg_true_state.gamma = gamma
+
+        Vg_horz = Vg * np.cos(gamma)
+        e1 = np.array([[1, 0, 0]]).T
+
+        chi = acos(np.dot(e1.T, Vg_horz) / np.linalg.norm(Vg_horz))
+        if(Vg_horz.item(1) < 0):
+            chi *= -1
+        # if(chi <= -np.pi):
+        #     chi += 2 * np.pi
+        self.msg_true_state.chi = chi
+
 
     def updateVelocityData(self, wind=np.zeros((6, 1))):
         Rb_v = Quaternion2Rotation(self._state[6:10]).T

chp6/mavsim_chp6.py
@@ -42,7 +42,8 @@ while sim_time < SIM.t_end:
     #-------controller-------------
     estimated_state = dyn.msg_true_state  # uses true states in the control
     commands.airspeed_command = Va_command.square(sim_time)
-    commands.course_command = chi_command.square(sim_time)
+    # commands.course_command = chi_command.square(sim_time)
+    commands.course_command = np.deg2rad(180)
     commands.altitude_command = h_command.square(sim_time)
     delta, commanded_state = ctrl.update(commands, estimated_state)
 