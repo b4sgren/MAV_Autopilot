e780a20126dd5a428fa23df6af69135a94dd5d6a
Brendon Forsgren <bforsgren29@gmail.com>
2019-03-20 13:20:51 -0600

    Path following is done just about. Now to test and tune

--- chp10/path_follower.py
+++ chp10/path_follower.py
@@ -9,7 +9,7 @@ class path_follower:
     def __init__(self):
         self.chi_inf = np.radians(80)   # approach angle for large distance from straight-line path
         self.k_path = 0.02  # proportional gain for straight-line path following
-        self.k_orbit =  0.5 # proportional gain for orbit following
+        self.k_orbit =  2.5 # proportional gain for orbit following
         self.gravity = 9.8
         self.autopilot_commands = msg_autopilot()  # message sent to autopilot
 
@@ -61,7 +61,7 @@ class path_follower:
         Vg = state.Vg
         psi = state.psi
         chi = state.chi
-        p = np.array([[state.pn, state.pe, -state.h]])
+        p = np.array([[state.pn, state.pe, -state.h]]).T
         d = p - path.orbit_center
         d_norm = np.linalg.norm(d)
         R = path.orbit_radius
